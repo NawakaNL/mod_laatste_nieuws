a4ba2b48b790563029d508733453d545936c1c26
Tristan de Boer <info@tristandeboer.nl>
2018-03-16 12:38:34 +0100

    Formatted date correctly

--- tmpl/default.php
+++ tmpl/default.php
@@ -20,7 +20,7 @@ defined('_JEXEC') or die;
 			<li itemscope itemtype="https://schema.org/Article">
 				<a href="<?php echo $item->link; ?>" itemprop="url">
 					<span class="date">
-						<?php echo JHtml::_('date', $item->created, 'H:i') ?>
+						<?php echo JHtml::_('date', $item->created, 'F j, H:i') ?>
 					</span>
 					<span itemprop="name">
 						<?php echo $item->title; ?>
